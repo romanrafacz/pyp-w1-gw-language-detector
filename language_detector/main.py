8235367417d3957c5b7e60de442c7ecf773c19b8
Roman Rafacz <romanr32@gmail.com>
2016-10-18 23:51:58 +0000

    added mising semicolon

--- language_detector/main.py
+++ language_detector/main.py
@@ -5,7 +5,7 @@ def detect_language(text, languages):
     """Returns the detected language of given text."""
     # implement your solution here
     lang_sums = {}
-    for language in languages
+    for language in languages:
         lang_sums[sum([text.count(" %s " %x) for x in language['common_words']])] = language['name']
     return lang_sums[max(lang_sums.keys())]
     #return lang_sums
